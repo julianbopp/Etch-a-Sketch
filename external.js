fa503678335666bf1c22ccd91da07b45702bb8b1
julianbopp <julian.bopp@tutanota.com>
2022-08-04 22:57:30 +0200

    add hover effect

--- external.js
+++ external.js
@@ -8,7 +8,13 @@ for (let i = 0; i < GRIDSIZE; i++) {
 
     for (let j = 0; j < GRIDSIZE; j++) {
         let div = document.createElement("div");
-        div.classList.add("gridElement")
-        row.appendChild(div)
+        div.classList.add("gridElement");
+        row.appendChild(div);
     }
 }
+
+
+let grid = document.querySelectorAll(".gridElement");
+grid.forEach(element => element.addEventListener("mouseenter", function() {this.classList.add("hover")}));
+
+grid.forEach(element => element.addEventListener("mouseleave", function() {this.classList.remove("hover")}));
