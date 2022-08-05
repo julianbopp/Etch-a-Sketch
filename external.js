0be4b1c849e1407c611f18755243f3e3538af73d
julianbopp <julian.bopp@tutanota.com>
2022-08-05 14:52:34 +0200

    add button that accepts new grid size

--- external.js
+++ external.js
@@ -1,21 +1,38 @@
-
-let GRIDSIZE = 16;
-
 let container = document.querySelector(".container");
-for (let i = 0; i < GRIDSIZE; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
 
-    for (let j = 0; j < GRIDSIZE; j++) {
-        let div = document.createElement("div");
-        div.classList.add("gridElement");
-        row.appendChild(div);
+function createGrid(GRIDSIZE) {
+    for (let i = 0; i < GRIDSIZE; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+
+        for (let j = 0; j < GRIDSIZE; j++) {
+            let div = document.createElement("div");
+            div.classList.add("gridElement");
+            row.appendChild(div);
+     }
     }
+    let grid = document.querySelectorAll(".gridElement");
+    grid.forEach(element => element.addEventListener("mouseenter", function() {this.classList.add("hover")}));
+
+    grid.forEach(element => element.addEventListener("mouseleave", function() {this.classList.remove("hover")}));
+
+}
+
+function removeGrid() {
+    container.replaceChildren();
 }
 
 
-let grid = document.querySelectorAll(".gridElement");
-grid.forEach(element => element.addEventListener("mouseenter", function() {this.classList.add("hover")}));
+function sizeButtonClick() {
+    let size = prompt("Input Grid Size");
+    GRIDSIZE = Number(size);
+    removeGrid();
+    createGrid(GRIDSIZE);
 
-grid.forEach(element => element.addEventListener("mouseleave", function() {this.classList.remove("hover")}));
+}
+
+createGrid(16);
+
+let sizeButton = document.querySelector(".sizeButton")
+sizeButton.addEventListener("click", sizeButtonClick);
